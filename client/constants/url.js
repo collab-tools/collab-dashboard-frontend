acf94294be8b5100f3afa84d0ddfed2eb2f67bee
Lei Yang <raymondlei90s@gmail.com>
2018-09-21 14:09:01 +0800

    change url port

--- client/constants/url.js
+++ client/constants/url.js
@@ -1,6 +1,6 @@
-export const ROOT_API = 'http://localhost:3001';
-export const AUTHENTICATE_USER_URL = '/api/admin/authenticate';
-export const GET_ALL_STAFF_URL = '/api/admin/all';
-export const CREATE_STAFF_URL = '/api/admin/';
-export const UPDATE_STAFF_URL = '/api/admin/';
-export const GET_STAFF_DETAILS_URL = '/api/admin/detail'
+export const ROOT_API = 'http://localhost:5000/api';
+export const AUTHENTICATE_USER_URL = '/admin/authenticate';
+export const GET_ALL_STAFF_URL = '/admin/all';
+export const CREATE_STAFF_URL = '/admin/';
+export const UPDATE_STAFF_URL = '/admin/';
+export const GET_STAFF_DETAILS_URL = '/admin/detail'
